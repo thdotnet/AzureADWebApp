9f15605436dc21d23fccfdad83080a773b2a7acd
thiago custodio <thiago custodio>
2016-10-29 09:30:11 -0200

    tests using Azure AD to secure Web App - MVC

---
+++ WebApplication2/App_Start/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Owin;
+
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+[assembly:OwinStartup(typeof(WebApplication2.App_Start.Startup))]
+namespace WebApplication2.App_Start
+{
+    public class Startup
+    {
+        private static string _clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string _aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string _tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string _postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        string _authority = String.Format(CultureInfo.InvariantCulture, _aadInstance, _tenant);
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions {
+                ClientId = _clientId,
+                Authority = _authority,
+                PostLogoutRedirectUri = _postLogoutRedirectUri,
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    AuthenticationFailed = context => {
+                        context.HandleResponse();
+                        context.Response.Redirect("/Error/message=" + context.Exception.Message);
+                        return Task.FromResult(0);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void Signin()
+        {
+            if(Request.IsAuthenticated == false)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                        new AuthenticationProperties { RedirectUri = "/" },
+                        OpenIdConnectAuthenticationDefaults.AuthenticationType
+                    );
+            }
+        }
+
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+
+        // GET: Account 
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
